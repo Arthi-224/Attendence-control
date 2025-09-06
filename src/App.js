702e4d02f9ca89c41812a096d9a2b2e213dc22bf
Arthi-224 <arthi.murugan2701@gmail.com>
2025-09-06 15:11:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Header from "./Header";
+import'./App.css'
+import'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useState }  from 'react'
+
+
+const App = () => {
+
+const total=29;
+  
+  const [present, setPresent] = useState([]);
+  const [input, setInput] = useState("");
+  const studentList = [
+    "Arun", "Aruvi", "Bella", "Balu", "Babu", "Camilia", "Chinnu", "Dravid",
+    "Dinesh", "Eva", "Fathima", "Fahad", "goki", "Kokila", "Hema", "Hemilia",
+    "Inthu", "Indhra", "Jaivel", "Jonny", "Lilly", "Moni", "Nivi", "Nainika",
+    "sneha", "subatra", "sumi", "Vinesh", "Yuva"
+  ];
+
+   const handleClick = () => {
+    
+    
+    if(input==""){
+      alert("Please enter your name")
+    }
+
+
+    const match=studentList.find ((list) =>list.toLowerCase()===input.toLowerCase());
+
+   if(!match){
+    alert("Not in the list")
+    
+   }
+    
+
+  if(present.includes(match)){
+    alert("Already marked present")
+
+  }
+
+  setPresent((prev)=>[...prev,match]);
+  setInput("") ;
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>UseState</h1>
+      <h5 className="atten">Attendence System</h5>
+
+      <div className="present">
+      <p><b>Present:{present.length}</b></p>
+      <p><b>Absent:{total-present.length}</b></p>
+      <p><b>Total:{total}</b></p>
+      </div>
+
+      <div className="box">
+    <div className="square">
+    <input type="text" id="name" placeholder="Enter Your Name" value={input} onChange={(e)=>setInput(e.target.value) }></input>
     </div>
-  );
-}
+    <div className="sub">
+    <button type="submit" id="submit" onClick={handleClick}>Submit</button>
+    
+   </div>
+   </div>
+
+    
+
+    <div className="tbl">
+    <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Check</th>
+        <th>StudentName</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+     
+      {studentList.map((student, index) => (
+        <>
+      <tr key={index}>
+        <td><input type="checkbox" checked={present.includes(student)}></input></td>
+        <td>{student}</td>
+        
+      </tr>
+      </>
+      
+      )
+    )
+  }  
+
+</tbody>
+</table>
+
+
+
+
+
+
+
+    
+    </div>
+    
+    </div>
+  )
+};
+
+
 
 export default App;
